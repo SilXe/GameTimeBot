bf431593b3937961d79f885d5a014adec99fb9bf
Sonbom Lee <161263824+SilXe@users.noreply.github.com>
2025-05-06 20:06:44 +0900

    Update game_aliases.py

--- utils/game_aliases.py
+++ utils/game_aliases.py
@@ -1,10 +1,41 @@
 GAME_ALIASES = {
+    # League of Legends
     "lol": "League of Legends",
     "league": "League of Legends",
+    "leagueoflegends": "League of Legends",
+    "league of legends": "League of Legends",
+
+    # VALORANT
     "valorant": "VALORANT",
     "val": "VALORANT",
+
+    # Minecraft
     "mc": "Minecraft",
     "minecraft": "Minecraft",
+
+    # Apex Legends
     "apex": "Apex Legends",
     "apex legends": "Apex Legends",
+
+    # Rainbow Six Siege
+    "r6": "Rainbow Six Siege",
+    "rainbow six": "Rainbow Six Siege",
+    "rainbow six siege": "Rainbow Six Siege",
+    "siege": "Rainbow Six Siege",
+
+    # Overwatch
+    "ow": "Overwatch",
+    "overwatch": "Overwatch",
+
+    # Escape from Tarkov
+    "eft": "Escape from Tarkov",
+    "tarkov": "Escape from Tarkov",
+    "escape from tarkov": "Escape from Tarkov",
+
+    # Civilization
+    "civ": "Civilization",
+    "civilization": "Civilization",
+    "civ 6": "Civilization",
+    "civ vi": "Civilization",
+
 }
