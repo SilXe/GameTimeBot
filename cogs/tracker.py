f69ce1086d7132267957abf9f182998d11e8b7f9
Sonbom Lee <161263824+SilXe@users.noreply.github.com>
2025-05-06 18:08:24 +0900

    Added XP bar
    Profile command shows XP bars
    Extracted XP and level to utils

--- cogs/tracker.py
+++ cogs/tracker.py
@@ -4,28 +4,9 @@ import discord
 from discord.ext import commands
 import datetime
 from db.database import users
+from utils.level import LEVEL_THRESHOLDS, calculate_level
 
 active_sessions = {}
-
-# Level thresholds in total seconds
-LEVEL_THRESHOLDS = [
-    300,        # Level 1: 5 min
-    900,        # Level 2: 15 min
-    1800,       # Level 3: 30 min
-    3600,       # Level 4: 1 hr
-    7200,       # Level 5: 2 hr
-    18000,      # Level 6: 5 hr
-    36000,      # Level 7: 10 hr
-    72000,      # Level 8: 20 hr
-    180000,     # Level 9: 50 hr
-    360000      # Level 10: 100 hr
-]
-
-def calculate_level(total_seconds):
-    for i in reversed(range(len(LEVEL_THRESHOLDS))):
-        if total_seconds >= LEVEL_THRESHOLDS[i]:
-            return i + 1
-    return 0
 
 class Tracker(commands.Cog):
     def __init__(self, bot):
