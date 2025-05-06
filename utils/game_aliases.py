1f98d8902617777af61c7b9c99a05839c69f2451
Sonbom Lee <161263824+SilXe@users.noreply.github.com>
2025-05-06 19:27:43 +0900

    Created Leaderboard
    Created command Leaderboard
    utils: game_aliases is used for user convinience in using command leaderboard for certain games

---
+++ utils/game_aliases.py
@@ -0,0 +1,10 @@
+GAME_ALIASES = {
+    "lol": "League of Legends",
+    "league": "League of Legends",
+    "valorant": "VALORANT",
+    "val": "VALORANT",
+    "mc": "Minecraft",
+    "minecraft": "Minecraft",
+    "apex": "Apex Legends",
+    "apex legends": "Apex Legends",
+}
